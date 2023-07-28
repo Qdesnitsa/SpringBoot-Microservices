c955b0249c8043416bc34a1876e8137bfeddd08c
Qdesnitsa <lenkasid@tut.by>
2023-07-28 23:34:20 +0300

    create OrganizationDto and OrganizationMapper

---
+++ organization-service/src/main/java/com/springboot/microservices/organizationservice/dto/OrganizationDto.java
@@ -0,0 +1,17 @@
+package com.springboot.microservices.organizationservice.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganizationDto {
+    private Long id;
+    private String organizationName;
+    private String organizationDescription;
+    private String organizationCode;
+    private LocalDateTime createdDate;
+}
