6438269c0bd47067cc5905f3a9174a0aea3fd959
Qdesnitsa <lenkasid@tut.by>
2023-07-28 23:25:46 +0300

    create organization jpa entity and spring data jpa repository

---
+++ organization-service/src/main/java/com/springboot/microservices/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.microservices.organizationservice.repository;
+
+import com.springboot.microservices.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
