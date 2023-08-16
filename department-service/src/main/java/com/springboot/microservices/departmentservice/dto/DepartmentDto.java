c873544906937770b53074eea6645a74b0600487
Qdesnitsa <lenkasid@tut.by>
2023-08-17 00:13:03 +0300

    customize swagger api documentation using annotations

--- department-service/src/main/java/com/springboot/microservices/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/com/springboot/microservices/departmentservice/dto/DepartmentDto.java
@@ -1,10 +1,14 @@
 package com.springboot.microservices.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "DepartmentDto Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,7 +16,19 @@ import lombok.Setter;
 public class DepartmentDto {
 
     private Long id;
+
+    @Schema(
+            description = "Department name"
+    )
     private String departmentName;
+
+    @Schema(
+            description = "Department description"
+    )
     private String departmentDescription;
+
+    @Schema(
+            description = "Department code"
+    )
     private String departmentCode;
 }
